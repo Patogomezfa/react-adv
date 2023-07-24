73c950779b3126b90d82fb952402e97f8492539d
patogomezfa <patogomezfa@gmail.com>
2023-07-24 08:35:28 -0300

    Control props

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { CSSProperties, ReactElement, createContext  } from 'react';
-import { ProductContextProps, Product } from '../interfaces/interfaces';
+import { ProductContextProps, Product, onChangeArgs } from '../interfaces/interfaces';
 import { useProduct } from '../hooks/useProduct'
 
 import styles from '../styles/styles.module.css';
@@ -8,15 +8,21 @@ export interface Props {
     product: Product,
     children?: ReactElement | ReactElement[],
     className?: string,
-    style?: CSSProperties
+    style?: CSSProperties,
+    onChange?: (args: onChangeArgs) => void,
+    value?: number
 }
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product, className, style }: Props ) => {
+export const ProductCard = ({ children, product, className, style, onChange, value }: Props ) => {
 
-    const { counter, increaseBy } = useProduct();
+    const { counter, increaseBy } = useProduct({
+        onChange,
+        product,
+        value
+    });
 
     return (
         <Provider value={{
--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { Product, onChangeArgs } from '../interfaces/interfaces'
 
-export const useProduct = () => {
+interface useProductArgs {
+    product: Product,
+    onChange?: (args: onChangeArgs) => void,
+    value?: number,
+}
 
-    const [counter, setCounter] = useState(0)
+export const useProduct = ({ onChange, product, value = 0 }: useProductArgs ) => {
 
-    const increaseBy = ( value: number) => {
-        setCounter( prev => Math.max(prev + value, 0))
+    const [counter, setCounter] = useState(value);
+
+    const isControlled = useRef( !!onChange )
+
+    useEffect(() => {
+        setCounter(value);
+    }, [value])
+    
+
+    const increaseBy = (value: number) => {
+
+        if (isControlled.current) {
+            return onChange!({count: value, product})
+        }
+
+        const newValue = Math.max(counter + value, 0);
+
+        setCounter(prev => Math.max(prev + value, 0))
+         
+        onChange && onChange({ count: newValue, product });
     }
 
     return {
